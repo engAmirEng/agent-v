f746618ab4047a1da6f3fb7ab41cb5577cd10318
AmirW <eng.amir.bu@gmail.com>
2023-07-12 18:17:56 +0330

    set admin panel titles

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.utils.translation import gettext_lazy
+
+admin.site.site_title = gettext_lazy("AgentV administration")
+admin.site.site_header = gettext_lazy("AgentV")
+admin.site.index_title = gettext_lazy("AgentV Admin")
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
