05f0ca1e3f20f2fef14249eedabd3f9c57af9ed1
Amir Khalife <eng.amir.bu@gmail.com>
2023-08-31 10:43:06 +0330

    sentry added

--- config/settings/third_party.py
+++ config/settings/third_party.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from ._setup import env
 
 # Celery
@@ -44,3 +47,23 @@ DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
+
+# Sentry
+# ------------------------------------------------------------------------------
+
+if env.bool("SENTRY_ENABLE", default=False):
+    sentry_sdk.init(
+        dsn=env.url("SENTRY_DSN"),
+        integrations=[DjangoIntegration()],
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
